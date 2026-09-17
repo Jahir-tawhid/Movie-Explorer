1a490e781f32f3b51509f0ba5fe3fe34dc39f1e5
Jahir-tawhid <jahirtawhid1988@gmail.com>
2026-09-17 14:52:06 +0600

    Updated project code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,3 @@
-// function Navbar({ onNavigateHome, onNavigateMovies }) {
-//   return (
-//     <nav className="flex justify-between items-center px-8 py-4 bg-gray-900 text-white shadow-md">
-//       <h1 className="text-xl font-bold cursor-pointer" onClick={onNavigateHome}>
-//         🎬 MovieExplorer
-//       </h1>
-
-//       <div className="flex gap-4">
-//         <button
-//           onClick={onNavigateHome}
-//           className="hover:text-blue-400 transition"
-//         >
-//           Home
-//         </button>
-//         <button
-//           onClick={onNavigateMovies}
-//           className="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700 transition"
-//         >
-//           Movies
-//         </button>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 function Navbar({ onNavigateHome, onNavigateMovies }) {
   return (
     // Top navigation bar
